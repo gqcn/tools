6e498266b7936c3b9e5f91406e27b81bfc13201d
txqiangguo <txqiangguo@tencent.com>
2024-03-14 14:23:50 +0800

    up

---
+++ prometheus_metrics_to_markdown_table/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/gogf/gf/v2/os/gfile"
+	"github.com/gogf/gf/v2/os/glog"
+	"github.com/gogf/gf/v2/text/gregex"
+	"os"
+)
+
+const (
+	mdTableHeader = `
+| 名称 | 类型 | 描述 |
+| ------ | ------ | ------ |
+`
+)
+
+func main() {
+	var ctx = context.Background()
+	if len(os.Args) < 2 {
+		glog.Fatal(ctx, `missing file path, usage eg: prometheus_metrics_to_markdown_table file.txt`)
+	}
+	var (
+		mdContent   string
+		fileContent = gfile.GetContents(os.Args[1])
+	)
+	// 生成markdown table
+	matches, err := gregex.MatchAllString(
+		`# HELP ([\s\S]+?)\s# TYPE (\w+) (\w+)`, fileContent,
+	)
+	if err != nil {
+		panic(err)
+	}
+	mdContent += mdTableHeader
+	for _, match := range matches {
+		mdContent += fmt.Sprintf("| %s | %s | %s |\n", match[1], match[3], match[2])
+	}
+	fmt.Println(mdContent)
+}
